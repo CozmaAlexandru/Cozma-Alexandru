01b55acfb817ca038fad8e6fef37434e6190d7a6
CozmaAlexandru <cozmaalex0901@gmail.com>
2025-11-12 15:19:48 +0200

    Refactor DDL for silver layer tables
    Updated data types for crm_prd_info and crm_sales_details tables.

--- Data Wearhouse Project/scripts/silver layer/ddl_silver.sql
+++ Data Wearhouse Project/scripts/silver layer/ddl_silver.sql
@@ -9,7 +9,6 @@ Script Purpose:
 ===============================================================================
 */
 
-
 DROP TABLE IF EXISTS silver.crm_cust_info;
 
 CREATE TABLE silver.crm_cust_info (
@@ -26,7 +25,8 @@ CREATE TABLE silver.crm_cust_info (
 DROP TABLE IF EXISTS silver.crm_prd_info;
 
 CREATE TABLE silver.crm_prd_info (
-    prd_idN INT,
+    prd_id INT,
+	cat_id VARCHAR(50),
     prd_key VARCHAR(50),
     prd_nm VARCHAR(50),
     prd_cost INT,
@@ -42,9 +42,9 @@ CREATE TABLE silver.crm_sales_details (
     sls_ord_num VARCHAR(50),
     sls_prd_key VARCHAR(50),
     sls_cust_id INT,
-    sls_order_dt INT,
-    sls_ship_dt INT,
-    sls_due_dt INT,
+    sls_order_dt DATE,
+    sls_ship_dt DATE,
+    sls_due_dt DATE,
     sls_sales INT,
     sls_quantity INT,
     sls_price INT,
